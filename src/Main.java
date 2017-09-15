0f7055d517337a773752313011dd3a3fa6708d91
Eric Abell <ericabell@users.noreply.github.com>
2017-09-15 12:30:15 -0500

    Prompt user for info on car in main complete

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	    // use Scanner to get VehicleInfo from the user
         // and initialize the POJO
 
+        Scanner scanner = new Scanner(System.in);
+
+        VehicleInfo newVehicle = new VehicleInfo();
+
+        System.out.println("Please enter the following info about your vehicle.");
+
+        System.out.println("VIN");
+        newVehicle.setVehicleIdentificationNumber(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println("Odometer");
+        newVehicle.setOdometer(Double.parseDouble(scanner.nextLine()));
+
+        System.out.println("Gallons of gas consumed");
+        newVehicle.setGallonsOfGasConsumed(Double.parseDouble(scanner.nextLine()));
+
+        System.out.println("Odometer at last oil change");
+        newVehicle.setOdometerAtLastOilChange(Double.parseDouble(scanner.nextLine()));
+
+        System.out.println("Engine size in liters");
+        newVehicle.setEngineSizeInLiters(Double.parseDouble(scanner.nextLine()));
+
         // call the report method from TelematicsService and pass
         // the VehicleInfo POJO
+
+        TelematicsService telematicsService = new TelematicsService();
+        telematicsService.report(newVehicle);
 
     }
 }
