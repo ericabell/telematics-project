e62a4f4ecd199248a406d5d65f375fbf6c99ee29
Eric Abell <ericabell@users.noreply.github.com>
2017-09-15 13:03:18 -0500

    Implement basic write of JSON data from new VehicleInfo object

--- src/TelematicsService.java
+++ src/TelematicsService.java
@@ -1,10 +1,35 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TelematicsService {
 
     void report(VehicleInfo vehicleInfo) {
         // Write VehicleInfo to a file as JSON using the VIN number with json extension
         // the file will overwrite any previous file with the same VIN
+        String filename = vehicleInfo.getVehicleIdentificationNumber() + ".json";
+        File newFile = new File(filename);
+
+        try {
+            FileWriter fileWriter = new FileWriter(newFile);
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(vehicleInfo);
+            fileWriter.write(json);
+            fileWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
         // find all files that end with json and convert back to VehicleInfo objects
+        File file = new File(".");
+        for (File f : file.listFiles()) {
+            if (f.getName().endsWith(".json")) {
+                // Now you have a File object named "f".
+                // You can use this to create a new instance of Scanner
+            }
+        }
 
         // update dashboard.html to show the data for all the VehicleInfo objects
 
